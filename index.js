bea9ba6287b05b06a029d20b95c99d0a0fdc2efe
Sarahwcode <sarahw181090@gmail.com>
2024-12-14 21:34:35 +0000

    made changes to landing page added css, java script and html

---
+++ index.js
@@ -0,0 +1,9 @@
+//function catName(event) {
+//event.preventDefault();
+//let formSubmit = document.querySelector("#form-text").value;
+//let h2 = document.createElement("h2");
+//h2.innerHTML = formSubmit;
+//}
+
+//let nameSubmit = document.querySelector("#form");
+//nameSubmit.addEventListener("submit", catName);
